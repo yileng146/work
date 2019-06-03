3eb15f18242d8fca5f8ab071d38fd05a69ba3c6e
yileng146 <ruiwang.chen@sharefamily.com.cn>
2019-06-03 22:34:49 +0800

    unity添加了玩家攻击方式，测试子弹飞行

---
+++ unity3d/ballgame/Assets/Assets/script/Player/PlayerAttact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttact : MonoBehaviour
+{
+    public Bullet bulletPerfab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))//当按下鼠标
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin,ray.direction*100,Color.white,10);//射线起始位置，方向，颜色，持续时间
+            RaycastHit raycastHit;
+            bool ishit = Physics.Raycast(ray,out raycastHit);
+            if (ishit)
+            {
+                print(raycastHit.collider.gameObject.name);              
+                print(raycastHit.point);                 
+                Vector3 dir = new Vector3(raycastHit.point.x, this.transform.position.y, raycastHit.point.z) - this.transform.position;
+                Ray moveray = new Ray(this.transform.position, dir);
+                Debug.DrawRay(moveray.origin, moveray.direction * 100, Color.green, 10);
+                //实例化子弹对象 
+                Bullet bullet = Instantiate(bulletPerfab);//生成预制体的源头和想生成的方向位置
+
+            }
+        }
+    }
+}
