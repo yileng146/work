122d5260ff4f05c898480c8c1948c42d60f2f9f2
yileng146 <ruiwang.chen@sharefamily.com.cn>
2019-06-04 09:45:57 +0800

    unity敌人生成成功

--- unity3d/ballgame/Assets/Assets/script/EnemyProduct.cs
+++ unity3d/ballgame/Assets/Assets/script/EnemyProduct.cs
@@ -20,17 +20,17 @@ public class EnemyProduct : MonoBehaviour
     {
         shouldProductEnemy(isSwitch);
         areabound = this.GetComponent<BoxCollider>().bounds;
-        InvokeRepeating("EnemySpawn",0.5f,2.0f);
+        InvokeRepeating("EnemySpawn",0.5F,2.0F);
     }
     private Vector3 RandomPlayerPosition()//随机生成玩家的位置 
     {
         float x = Random.Range(areabound.min.x,areabound.max.x);
         float y = 0.5f;
-        float z = Random.Range(areabound.min.x,areabound.max.y);
+        float z = Random.Range(areabound.min.z,areabound.max.z);
 
         return new Vector3(x,y,z);
     }
-    private void EnemySpan() {
+    private void EnemySpawn() {
         int idnex = Random.Range(0, enemiesPrefab.Length);
         Enemy enemy = Instantiate(enemiesPrefab[idnex], RandomPlayerPosition(), Quaternion.identity);
         enemy.init(Random.Range(3, 6), Random.Range(1, 3), player.transform);
