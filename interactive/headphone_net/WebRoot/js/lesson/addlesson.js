4ec284b365ce0d7185cd18c55699aa4e02b5c55b
yileng146 <ruiwang.chen@sharefamily.com.cn>
2019-06-05 20:40:34 +0800

    人机交互添加了添加课程模块

---
+++ interactive/headphone_net/WebRoot/js/lesson/addlesson.js
@@ -0,0 +1,10 @@
+var ue=UE.getEditor('lessonsEditor');
+
+function add_lesson(){
+	
+	var lesson_name=document.getElementById("lesson_name");
+	var lesson_class=document.getElementById("lesson_class");
+	var lesson_author=document.getElementById("lesson_author");
+	var lesson_price=document.getElementById("lesson_price");
+	
+}
