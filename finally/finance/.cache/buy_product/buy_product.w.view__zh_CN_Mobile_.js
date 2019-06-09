49b3582a025144308a0752f3726e1d177ea7469d
yileng146 <ruiwang.chen@sharefamily.com.cn>
2019-06-10 02:53:50 +0800

    ceshi

---
+++ finally/finance/.cache/buy_product/buy_product.w.view__zh_CN_Mobile_.js
@@ -0,0 +1,28 @@
+window.__justep.__ResourceEngine.loadCss([{url: '/UI2/v_3a9903390c5c4d27b74f01c0178d1222l_zh_CNs_d_m/system/components/bootstrap.min.css', include: '$model/UI2/system/components/bootstrap/lib/css/bootstrap,$model/UI2/system/components/bootstrap/lib/css/bootstrap-theme'},{url: '/UI2/v_e9fd32af74734bf6bdbcc5de60ded433l_zh_CNs_d_m/system/components/comp.min.css', include: '$model/UI2/system/components/justep/lib/css2/dataControl,$model/UI2/system/components/justep/input/css/datePickerPC,$model/UI2/system/components/justep/messageDialog/css/messageDialog,$model/UI2/system/components/justep/lib/css3/round,$model/UI2/system/components/justep/input/css/datePicker,$model/UI2/system/components/justep/row/css/row,$model/UI2/system/components/justep/dataTables/css/responsive,$model/UI2/system/components/justep/attachment/css/attachment,$model/UI2/system/components/justep/barcode/css/barcodeImage,$model/UI2/system/components/bootstrap/dropdown/css/dropdown,$model/UI2/system/components/justep/contents/css/contents,$model/UI2/system/components/justep/common/css/forms,$model/UI2/system/components/justep/dataTables/css/responsive,$model/UI2/system/components/justep/locker/css/locker,$model/UI2/system/components/justep/menu/css/menu,$model/UI2/system/components/justep/scrollView/css/scrollView,$model/UI2/system/components/justep/loadingBar/loadingBar,$model/UI2/system/components/justep/dialog/css/dialog,$model/UI2/system/components/justep/bar/css/bar,$model/UI2/system/components/justep/popMenu/css/popMenu,$model/UI2/system/components/justep/lib/css/icons,$model/UI2/system/components/justep/lib/css4/e-commerce,$model/UI2/system/components/justep/toolBar/css/toolBar,$model/UI2/system/components/justep/popOver/css/popOver,$model/UI2/system/components/justep/panel/css/panel,$model/UI2/system/components/bootstrap/carousel/css/carousel,$model/UI2/system/components/justep/wing/css/wing,$model/UI2/system/components/bootstrap/scrollSpy/css/scrollSpy,$model/UI2/system/components/justep/titleBar/css/titleBar,$model/UI2/system/components/justep/lib/css1/linear,$model/UI2/system/components/justep/numberSelect/css/numberList,$model/UI2/system/components/justep/list/css/list,$model/UI2/system/components/justep/dataTables/css/dataTables'}]);window.__justep.__ResourceEngine.loadJs(['/v_52dc3e6bc08641ffb54e175aed7e2022l_zh_CNs_d_m/system/core.min.js','/v_f366cf2386dc450fbf60289c1ddd568cl_zh_CNs_d_m/system/common.min.js','/v_95f8d4ca3a0b4253842e39c582ebe862l_zh_CNs_d_m/system/components/comp.min.js']);define(function(require){
+require('$model/UI2/system/components/justep/model/model');
+require('$model/UI2/system/components/justep/loadingBar/loadingBar');
+require('$model/UI2/system/components/justep/button/button');
+require('$model/UI2/system/components/justep/row/row');
+require('$model/UI2/system/components/justep/input/input');
+require('$model/UI2/system/components/justep/list/list');
+require('$model/UI2/system/components/justep/titleBar/titleBar');
+require('$model/UI2/system/components/justep/panel/child');
+require('$model/UI2/system/components/justep/data/data');
+require('$model/UI2/system/components/justep/window/window');
+require('$model/UI2/system/components/justep/panel/panel');
+var __parent1=require('$model/UI2/system/lib/base/modelBase'); 
+var __parent0=require('$model/UI2/finally/finance/buy_product'); 
+var __result = __parent1._extend(__parent0).extend({
+	constructor:function(contextUrl){
+	this.__sysParam='true';
+	this.__contextUrl=contextUrl;
+	this.__id='';
+	this.__cid='czEBfIv';
+	this._flag_='434f6d40949baa856afc1d7797a60eec';
+	this._wCfg_={};
+	this._appCfg_={};
+	this.callParent(contextUrl);
+ var __Data__ = require("$UI/system/components/justep/data/data");new __Data__(this,{"autoLoad":true,"confirmDelete":true,"confirmRefresh":true,"defCols":{"p_id":{"define":"p_id","name":"p_id","relation":"p_id","rules":{"integer":true},"type":"Integer"},"p_qixian":{"define":"p_qixian","name":"p_qixian","relation":"p_qixian","rules":{"integer":true},"type":"Integer"},"p_rongziguimo":{"define":"p_rongziguimo","name":"p_rongziguimo","relation":"p_rongziguimo","rules":{"integer":true},"type":"Integer"},"p_salenum":{"define":"p_salenum","name":"p_salenum","relation":"p_salenum","rules":{"integer":true},"type":"Integer"},"p_time":{"define":"p_time","name":"p_time","relation":"p_time","rules":{"date":true},"type":"Date"},"p_yuqishouyi":{"define":"p_yuqishouyi","name":"p_yuqishouyi","relation":"p_yuqishouyi","rules":{"number":true},"type":"Float"}},"directDelete":false,"events":{"onCustomRefresh":"finance_productsCustomRefresh"},"idColumn":"p_id","isMain":false,"limit":20,"xid":"finance1"});
+ new __Data__(this,{"autoLoad":true,"confirmDelete":true,"confirmRefresh":true,"defCols":{"p_id":{"define":"p_id","name":"p_id","relation":"p_id","type":"String"},"u_blance":{"define":"u_blance","name":"u_blance","relation":"u_blance","rules":{"number":true},"type":"Float"},"u_id":{"define":"u_id","name":"u_id","relation":"u_id","type":"String"},"u_in":{"define":"u_in","name":"u_in","relation":"u_in","rules":{"number":true},"type":"Float"},"u_password":{"define":"u_password","name":"u_password","relation":"u_password","type":"String"}},"directDelete":false,"events":{"onCustomRefresh":"user_infCustomRefresh"},"idColumn":"u_id","isMain":false,"limit":20,"xid":"user1"});
+}}); 
+return __result;});
---
+++ finally/finance/.release/buy_product/buy_product.w.form.js
@@ -0,0 +1,28 @@
+define(function(require){
+require('$model/UI2/system/components/justep/model/model');
+require('$model/UI2/system/components/justep/loadingBar/loadingBar');
+require('$model/UI2/system/components/justep/button/button');
+require('$model/UI2/system/components/justep/row/row');
+require('$model/UI2/system/components/justep/input/input');
+require('$model/UI2/system/components/justep/list/list');
+require('$model/UI2/system/components/justep/titleBar/titleBar');
+require('$model/UI2/system/components/justep/panel/child');
+require('$model/UI2/system/components/justep/data/data');
+require('$model/UI2/system/components/justep/window/window');
+require('$model/UI2/system/components/justep/panel/panel');
+var __parent1=require('$model/UI2/system/lib/base/modelBase'); 
+var __parent0=require('$model/UI2/finally/finance/buy_product'); 
+var __result = __parent1._extend(__parent0).extend({
+	constructor:function(contextUrl){
+	this.__sysParam='true';
+	this.__contextUrl=contextUrl;
+	this.__id='';
+	this.__cid='czEBfIv';
+	this._flag_='434f6d40949baa856afc1d7797a60eec';
+	this._wCfg_={};
+	this._appCfg_={};
+	this.callParent(contextUrl);
+ var __Data__ = require("$UI/system/components/justep/data/data");new __Data__(this,{"autoLoad":true,"confirmDelete":true,"confirmRefresh":true,"defCols":{"p_id":{"define":"p_id","name":"p_id","relation":"p_id","rules":{"integer":true},"type":"Integer"},"p_qixian":{"define":"p_qixian","name":"p_qixian","relation":"p_qixian","rules":{"integer":true},"type":"Integer"},"p_rongziguimo":{"define":"p_rongziguimo","name":"p_rongziguimo","relation":"p_rongziguimo","rules":{"integer":true},"type":"Integer"},"p_salenum":{"define":"p_salenum","name":"p_salenum","relation":"p_salenum","rules":{"integer":true},"type":"Integer"},"p_time":{"define":"p_time","name":"p_time","relation":"p_time","rules":{"date":true},"type":"Date"},"p_yuqishouyi":{"define":"p_yuqishouyi","name":"p_yuqishouyi","relation":"p_yuqishouyi","rules":{"number":true},"type":"Float"}},"directDelete":false,"events":{"onCustomRefresh":"finance_productsCustomRefresh"},"idColumn":"p_id","isMain":false,"limit":20,"xid":"finance1"});
+ new __Data__(this,{"autoLoad":true,"confirmDelete":true,"confirmRefresh":true,"defCols":{"p_id":{"define":"p_id","name":"p_id","relation":"p_id","type":"String"},"u_blance":{"define":"u_blance","name":"u_blance","relation":"u_blance","rules":{"number":true},"type":"Float"},"u_id":{"define":"u_id","name":"u_id","relation":"u_id","type":"String"},"u_in":{"define":"u_in","name":"u_in","relation":"u_in","rules":{"number":true},"type":"Float"},"u_password":{"define":"u_password","name":"u_password","relation":"u_password","type":"String"}},"directDelete":false,"events":{"onCustomRefresh":"user_infCustomRefresh"},"idColumn":"u_id","isMain":false,"limit":20,"xid":"user1"});
+}}); 
+return __result;});
