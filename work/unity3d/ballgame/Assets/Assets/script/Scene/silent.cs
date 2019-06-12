05319ccaaa87e3a5cf2469e4d2ca20f04eeb1a0c
yileng146 <ruiwang.chen@sharefamily.com.cn>
2019-06-12 17:43:33 +0800

    实验4测试setting按钮

---
+++ work/unity3d/ballgame/Assets/Assets/script/Scene/silent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class silent : MonoBehaviour
+{
+    public GameObject silent_btn;
+    public GameObject setting_btn;
+    public GameObject MusicControl;
+    public void OnStartGame(int SceneName)
+    {
+        SceneManager.LoadScene(SceneName);//读取场景
+    }
+
+    public void OnClick()
+    {
+        setting_btn.SetActive(false);
+        silent_btn.SetActive(false);
+        MusicControl.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
